86e0a8ac5a76c16118b618dbd1c282e5294f71b0
JosephatNgugi <josephatn10@gmail.com>
2024-03-13 16:41:57 +0300

    return an array of student objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
+const student5 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+export default function getListStudents() {
+  return [student1, student2, student5];
+}
