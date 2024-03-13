bc60ec2a405bd706ee305a8e4d7e62dbed48e52b
JosephatNgugi <josephatn10@gmail.com>
2024-03-13 17:14:39 +0300

    rewrite getListStudents function to a use arrow functions add test function for getListStudents and track project config files.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,7 @@ const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
 const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
 const student5 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
 
-export default function getListStudents() {
-  return [student1, student2, student5];
-}
+const studentArray = [student1, student2, student5];
+const getListStudents = () => studentArray;
+
+export default getListStudents;
